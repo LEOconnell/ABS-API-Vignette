8bc939f5f50d03d67f2df9164c531b4e9a7442ea
Lisa OConnell <loconne2@ncsu.edu>
2023-10-03 12:32:24 -0400

    Testing Git Hub Page Render

---
+++ CreateREADME.R
@@ -0,0 +1,9 @@
+#Lisa O'Connell
+#Purpose: To process the ABSVignette.Rmd to a README.md file to display a GitHub Page
+
+
+rmarkdown::render(input="GitPageTest.Rmd", 
+                  output_file = "README.md",
+                  output_format = "github_document"
+                  )
+
